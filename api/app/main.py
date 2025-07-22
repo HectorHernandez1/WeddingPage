18cc9ef50be563acc52c728224559bc9523501a0
hectorhernandez <hhhector9@gmail.com>
2025-07-22 11:54:15 -0700

    clean up the API and remove unwated code

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends, Request
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, constr, Field, validator
+from pydantic import BaseModel, constr, validator
 from typing import Optional, Union
 import re
 from database import get_db
